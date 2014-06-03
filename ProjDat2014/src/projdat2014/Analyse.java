589f12fe6bbffc6edefd8b632a8b21a34b5efe96
Mathias Fall <mathias.fall@gmail.com>
2014-06-03 22:30:27 +0200

    R

---
+++ ProjDat2014/src/projdat2014/Analyse.java
@@ -0,0 +1,50 @@
+
+package projdat2014;
+
+import java.io.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.util.*;
+import java.util.regex.*;
+
+public class Analyse {
+  
+ public static void main(String[] args){
+ PDDocument pd;
+ String att;
+ try { 
+         //  PDF fil der skal læses
+         File input = new File("/home/fall/Desktop/proto2.pdf"); 
+
+         // StringBuilder opbevarer den udtrukkede tekst
+         StringBuilder sb = new StringBuilder();
+         pd = PDDocument.load(input);
+         PDFTextStripper stripper = new PDFTextStripper();
+
+         // Tilføjer text til StringBuilder
+         sb.append(stripper.getText(pd));
+
+         // Udtrækker relevant data fra pdf vha. regex.
+         // Regex syntax kan findes her:
+         // http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
+         // Leder efter linjer omsluttet af "//...//"
+         Pattern p = Pattern.compile("//\\S+//");
+
+         // Matcher, der leder i texten
+         Matcher m = p.matcher(sb);
+
+         while (m.find()){
+             // group() metoden sætter teksten ind i variablen att.
+             att = m.group();
+             System.out.println(att);
+         }
+
+         if (pd != null) {
+             pd.close();
+         }
+ } catch (Exception e){
+         e.printStackTrace();
+        }
+     }
+
+     } 
+
---
+++ ProjDat2014/src/projdat2014/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projdat2014;
+
+/**
+ *
+ * @author fall
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI form = new GUI();
+        form.setVisible(true);
+    }
+}
